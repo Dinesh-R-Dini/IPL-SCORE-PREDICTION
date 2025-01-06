10049d5e9e265759cc3eeb638312ea725b089ccc
Dinesh-R-Dini <dineshr112000@gmail.com>
2025-01-06 06:43:52 +0530

    Add files via upload

---
+++ MI/index.php
@@ -0,0 +1,206 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="UTF-8">
+    <title> MI TEAM SQUAD</title>
+    <link rel="stylesheet" href="style.css">
+   </head>
+<body>
+  <div class="container">
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img1.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img1.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Rohit Sharma</div>
+            <div class="job"> Batsman || Right-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img2.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img2.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Quinton de Kock</div>
+            <div class="job">Wicketkeeper Batsman || Left-handed </div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img3.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img3.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Trent Boult</div>
+            <div class="job"> Bowler || Right-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img4.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img4.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Piyush Chawala</div>
+            <div class="job">Bowler || Left-handed</div>
+
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img5.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img5.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Kieron Pollard</div>
+            <div class="job">All-rounder || Right-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img6.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img6.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Ishan Kishan</div>
+            <div class="job">Batsman || Left-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img7.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img7.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Jasprit Bumrah</div>
+            <div class="job"> Bowler|| Right-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img8.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img8.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Suryakumar Yadav</div>
+            <div class="job">Batsman || Right-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img9.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img9.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Krunal Pandya</div>
+            <div class="job">All-rounder || Left-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+          <div class="icon">
+            <img src="images/img10.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img10.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">Rahul Chahar</div>
+            <div class="job">Bowler || Right-handed</div>
+          </div>
+        </div>
+        </div>
+    </div>
+    <div class="icon-image">
+      <div class="icon">
+        <img src="images/img11.jpg" alt="">
+      </div>
+    <div class="hover-image one">
+      <div class="img">
+        <img src="images/img11.jpg" alt="">
+      </div>
+    <div class="content">
+      <div class="details">
+        <div class="name">Hardik Pandya</div>
+        <div class="job">All-rounder || Right-handed</div>
+      </div>
+    </div>
+    </div>
+</div>
+    <div class="icon-image last">
+          <div class="icon">
+            <img src="images/img12.jpg" alt="">
+          </div>
+        <div class="hover-image one">
+          <div class="img">
+            <img src="images/img12.jpg" alt="">
+          </div>
+        <div class="content">
+          <div class="details">
+            <div class="name">MUMBAI INDIANS</div>
+        
+          </div>
+        </div>
+        </div>
+    </div>
+  </div>
+</body>
+</html>
